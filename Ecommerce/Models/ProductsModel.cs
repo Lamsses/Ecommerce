b0addad2050122a867a3732010356ffaff6cdcd8
mohaymen <mohaymen@gmail.com>
2023-02-21 21:46:00 +0300

    Adding the products ,categrioes  and users table  with stored procedures for products

---
+++ Ecommerce/Models/ProductsModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace EcommerceLibrary.Models;
+
+public class ProductsModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+    public string? Price { get; set; }
+
+    public string? ImgUrl { get; set; }
+
+
+}
+
---
+++ EcommerceApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using EcommerceLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EcommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductsModel>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProductsModel> Get(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
