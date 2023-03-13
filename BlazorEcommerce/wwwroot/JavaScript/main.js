4154a8675a773dd3c53ceefd2b78ca176d160432
Jafar <jaafaroday22@gmail.com>
2023-03-13 23:06:27 +0300

    Fix the Errors

--- BlazorEcommerce/wwwroot/JavaScript/main.js
+++ BlazorEcommerce/wwwroot/JavaScript/main.js
@@ -1,38 +1,61 @@
-alert("Heloooooooo");
-console.log("this is working just fine");
-let taps = document.querySelectorAll("li .tap");
-let tapsArray = Array.from(taps);
-tapsArray.forEach((ele) => {
-    ele.addEventListener("click", function (e) {
 
-        tapsArray.forEach((ele) => {
-            ele.classList.remove("active");
-        })
-        e.currentTarget.classList.add("active");
-    })
-})
+//console.log("this is working just fine");
+function SelectTap() {
+    let taps = document.querySelectorAll("li .tap");
+    let tapsArray = Array.from(taps);
+    tapsArray.forEach((ele) => {
+        ele.addEventListener("click",
+            function(e) {
 
-var collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-    item.addEventListener("click", function () {
-        alert("Categoryyyyy");
-      this.classList.toggle("collapsible--expanded");
-      
-  })
-);
+                tapsArray.forEach((ele) => {
+                    ele.classList.remove("active");
+                });
+                e.currentTarget.classList.add("active");
+            });
+    });
+
+}
+
+function ShowCategory() {
+    const collapsible = document.querySelector(".collapsible");
+    collapsible.classList.toggle("collapsible--expanded");
+//    console.log(collapsible);
+//    collapsible.addEventListener("click",
+//        function() {
+//            this.classList.toggle("collapsible--expanded");
+//
+//        });
+
+}
+//let collapsibles = document.querySelectorAll(".collapsible");
+//collapsibles.forEach((item) =>
+//    item.addEventListener("click",
+//        function () {
+//            this.classList.toggle("collapsible--expanded");
+//
+//        })
+//);
+
+
+function ShowCart() {
+    var cart = document.querySelector(".cart");
+    cart.classList.toggle("toggle-cart");
+}
 var cart = document.querySelector(".cart");
-cart.addEventListener("click", function () {
-    this.classList.toggle("toggle-cart");
-  })
+cart.addEventListener("click",
+    function() {
+        this.classList.toggle("toggle-cart");
+    });
 
-var mainImg = document.getElementById('main-img');
-//var m = mainImg.src;
-var bottomImg = document.getElementsByClassName('bottom-img');
-let imgs = document.querySelectorAll(".bottom-img-container img");
-let imsgArray = Array.from(imgs);
-console.log(imsgArray);
-imsgArray.forEach((ele) => {
-    ele.addEventListener("click", function (e) {
-        mainImg.src = e.currentTarget.src;
-    })
-})
+//var mainImg = document.getElementById('main-img');
+////var m = mainImg.src;
+//var bottomImg = document.getElementsByClassName('bottom-img');
+//let imgs = document.querySelectorAll(".bottom-img-container img");
+//let imsgArray = Array.from(imgs);
+//console.log(imsgArray);
+//imsgArray.forEach((ele) => {
+//    ele.addEventListener("click",
+//        function(e) {
+//            mainImg.src = e.currentTarget.src;
+//        });
+//});
