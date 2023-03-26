1e2b9b4d444270e41f4dad4d950d607d38398f7e
Jafar <jaafaroday22@gmail.com>
2023-03-26 23:23:16 +0300

    1- Fix Some Responsive Problems 2- Make Categories Tap Active on Click 3- Make the footer shows in every page 4- Add Register/Login Form 5- Fix h1 ugly border

--- BlazorEcommerce/Pages/MainBase.cs
+++ BlazorEcommerce/Pages/MainBase.cs
@@ -10,6 +10,8 @@ public class MainBase : ComponentBase
     [Inject] public NavigationManager? NavigationManager { get; set; }
     [Inject] protected ILocalStorageService? LocalStorage { get; set; }
     [Inject] protected AuthenticationStateProvider? AuthStateProvider { get; set; }
+     protected AuthenticationModel Authenticat = new();
+
 
 
 
