6d4a52dc4ec828d452292875691507c19933908d
mohaymen <mohaymen@gmail.com>
2023-05-01 21:51:32 +0300

    س

--- BlazorEcommerce/Pages/Checkout.razor.cs
+++ BlazorEcommerce/Pages/Checkout.razor.cs
@@ -136,7 +136,8 @@ partial class Checkout
                 if (item.discounted_price > 0)
                 {
                     // var newPrice = (Convert.ToDecimal(item.discounted_price) * Convert.ToDecimal(item.ProductAmount));
-                    total += item.discounted_price;
+                    total += item.discounted_price * item.ProductAmount;
+                   
                 }
                 else
                 {
